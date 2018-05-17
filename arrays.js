304af0a746da521d55ce0700b45378b797693aac
Michael Iasiello <maiasiel@ncsu.edu>
2018-05-17 01:54:04 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -4,7 +4,7 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
 //add element
 function addElementToBeginningOfArray(array, element) {
-  var newArray = [...`${element}`, array[1]]
+  var newArray = [element, array]
   return newArray
 }
 
