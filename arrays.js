e63845b2e752ab0bf38f7dd5cfdb5e16d0597e32
Michael Iasiello <maiasiel@ncsu.edu>
2018-05-17 02:09:25 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -18,3 +18,8 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
 function accessElementInArray(array, index) {
   return array[index]
 }
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift()
+  return array
+}
